c01649b9290d44c62371d29e216314aabafb4fba
Harsh Karan <karanharshe577@gmail.com>
2024-03-07 12:05:53 +0530

    header component added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,13 @@ import About from "./pages/About";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
