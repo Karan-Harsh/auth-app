2bd8270ebbde5355b2a44487b2641bdc125e56ad
Harsh Karan <karanharshe577@gmail.com>
2024-03-07 12:12:45 +0530

    connected to database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Databse");
+  })
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
