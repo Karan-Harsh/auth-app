db82cb10c818ee41c3375e2eb2b0efa78ad270bf
Harsh Karan <karanharshe577@gmail.com>
2024-03-07 12:27:41 +0530

    create testapi routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "../routes/user.routes.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,8 @@ mongoose
     console.log("Connected to Databse");
   })
   .catch((err) => console.log(err));
+
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
