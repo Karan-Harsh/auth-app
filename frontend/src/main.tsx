b00a52abff00ea0e97f4927d2d5edc2f99240ddb
Harsh Karan <karanharshe577@gmail.com>
2024-03-18 15:13:03 +0530

    chat ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { RecoilRoot } from "recoil";
+import { Chat } from "./components/Chat.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+  // <RecoilRoot>
+  //   <App />
+  // </RecoilRoot>
+  <Chat />
 );
